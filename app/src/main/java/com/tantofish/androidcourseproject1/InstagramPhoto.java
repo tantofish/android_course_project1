8fbd1c6a91cbfdd449beb3657d1647df827028ef
Yu Tu <yutu@yahoo-inc.com>
2015-07-24 18:44:27 +0800

    basic requirements

---
+++ app/src/main/java/com/tantofish/androidcourseproject1/InstagramPhoto.java
@@ -0,0 +1,13 @@
+package com.tantofish.androidcourseproject1;
+
+/**
+ * Created by yutu on 7/24/15.
+ */
+public class InstagramPhoto {
+    public String username;
+    public String caption;
+    public String imageUrl;
+    public int imageHeight;
+    public int imageWidth;
+    public int likeCount;
+}
--- app/src/main/java/com/tantofish/androidcourseproject1/MainActivity.java
+++ app/src/main/java/com/tantofish/androidcourseproject1/MainActivity.java
@@ -1,18 +1,88 @@
 package com.tantofish.androidcourseproject1;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ListView;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.apache.http.Header;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    static final String CLIENT_ID = "2dd2f932e83f4333bfb15b08c34c908e";
+    private ArrayList<InstagramPhoto> photos;
+    private InstagramPhotoAdapter ipAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        photos = new ArrayList<>();
+        ipAdapter = new InstagramPhotoAdapter(this, photos);
+
+        ListView lvPhotos = (ListView) findViewById(R.id.lvPhotos);
+        lvPhotos.setAdapter(ipAdapter);
+
+        //send request to instagram API
+        fetchPopularPhoto();
+
+
     }
+
+    public void fetchPopularPhoto(){
+        // Popular: https://api.instagram.com/v1/media/popular?access_token=ACCESS-TOKEN
+        // Type: { "data" => [x] => "type" } (image or video)
+        // URL: { "data" => [x] => "images" => "standard_resolution" => "url"}
+        // Caption: { "data" => [x] => "caption" => "text"}
+        // Author: { "data" => [x] => "user" => "username"}
+
+        String url = "https://api.instagram.com/v1/media/popular?client_id=" + CLIENT_ID;
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.get(url, null, new JsonHttpResponseHandler() {
+            // onSuccess(worked, 200)
+
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                //super.onSuccess(statusCode, headers, response);
+
+                JSONArray photosJson = null;
+                try {
+                    photosJson = response.getJSONArray("data");
+                    for( int i = 0 ; i < photosJson.length() ; i++){
+                        JSONObject photoJson = photosJson.getJSONObject(i);
+                        InstagramPhoto photo = new InstagramPhoto();
+                        photo.username    = photoJson.getJSONObject("user").getString("username");
+                        photo.caption     = photoJson.getJSONObject("caption").getString("text");
+                        photo.imageUrl    = photoJson.getJSONObject("images").getJSONObject("standard_resolution").getString("url");
+                        photo.imageHeight = photoJson.getJSONObject("images").getJSONObject("standard_resolution").getInt("height");
+                        photo.imageWidth  = photoJson.getJSONObject("images").getJSONObject("standard_resolution").getInt("width");
+                        photo.likeCount   = photoJson.getJSONObject("likes").getInt("count");
+
+                        photos.add(photo);
+                    }
+                } catch (JSONException e){
+                    e.printStackTrace();
+                }
+
+                ipAdapter.notifyDataSetChanged();
+            }
+
+
+            // onFailure()
+        });
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
